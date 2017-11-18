257897b086dce27199fae7628ac6abbf7dc1e72d
veckenz <veckenz@gmail.com>
2017-11-18 16:01:01 +0700

    fix bug error read text

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const queue = async.queue((task, callback) => {
     request(task.url, (error, response, body) => {
         const $ = cheerio.load(body)
         const text = $('#mw-content-text p').text()
-        fs.writeFile(task.name +'.txt' , body, (err) => {
+        fs.writeFile(task.name +'.txt' , text, (err) => {
             if(err){
                 console.log(err)
                 callback()
